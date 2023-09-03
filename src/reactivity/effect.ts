e1b88db698cf6c8dfca8e0f4267603c4a84488c6
那可心 <377079288@qq.com>
2023-09-03 23:36:06 +0800

    实现 effect 返回 runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -7,7 +7,7 @@ class ReactiveEffect {
 
 	run() {
 		activeEffect = this;
-		this._fn();
+		return this._fn();
 	}
 }
 
@@ -39,7 +39,10 @@ export function trigger(target, key) {
 
 let activeEffect;
 export function effect(fn) {
+	// fn
 	const _effect = new ReactiveEffect(fn);
 
 	_effect.run();
+
+	return _effect.run.bind(_effect);
 }
